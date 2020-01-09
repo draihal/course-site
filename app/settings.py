8851b8c4cf906a9eb78130d454f8b01b677eb86d
Draihal <draihal.a@gmail.com>
2020-01-09 14:19:31 +0300

    Add redis and celery.

--- app/settings.py
+++ app/settings.py
@@ -181,8 +181,8 @@ DJOSER = {
 }
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
